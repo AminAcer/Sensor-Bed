c09db622692d0e6b4c97eb38d6ffbc64f4ba2a2a
Your Name <aminvatsal1999@gmail.com>
2025-02-22 23:22:31 -0600

    defining LED PING

--- main/main.c
+++ main/main.c
@@ -3,14 +3,23 @@
 #include <freertos/task.h>
 
 #include "esp_log.h"
+#include "driver/gpio.h"
+
+#define WHITE_LED 35
 
 void app_main(void)
 {
   char* ourTaskName = pcTaskGetName(NULL);
   ESP_LOGI(ourTaskName, "Hello...\n");
 
+  gpio_reset_pin(WHITE_LED);
+  gpio_set_direction(WHITE_LED, GPIO_MODE_OUTPUT);
+
   while(1)
   {
+    gpio_set_level(WHITE_LED, 0);
+    vTaskDelay(1000/portTICK_PERIOD_MS);
+    gpio_set_level(WHITE_LED, 1);
     vTaskDelay(1000/portTICK_PERIOD_MS);
   }
 
