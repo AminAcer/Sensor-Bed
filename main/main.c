b4efb760723d549670a69171376a77458fd41a1c
Your Name <aminvatsal1999@gmail.com>
2025-02-23 02:50:38 -0600

    adding initial display logic, needs tweeking

--- main/main.c
+++ main/main.c
@@ -1,25 +1,218 @@
 #include <stdio.h>
+#include <unistd.h>
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
-
-#include "esp_log.h"
 #include "driver/gpio.h"
+#include "esp_log.h"
+#include "esp_lcd_panel_io.h"
+#include "esp_lcd_panel_ops.h"
+#include "esp_lcd_panel_dev.h"
+#include "esp_lcd_panel_ssd1306.h"
+#include "esp_timer.h"
+#include "esp_err.h"
+#include "esp_log.h"
+#include "driver/i2c_master.h"
+#include "lvgl.h"
 
-#define WHITE_LED 35
+#include "pins.h"
+
+static uint8_t oled_buffer[DISPLAY_HEIGHT * DISPLAY_WIDTH / 8];
+static _lock_t lvgl_api_lock;
+static const char *LOG_TAG = "LOG";
+
+static void example_lvgl_flush_cb(lv_display_t *disp, const lv_area_t *area, uint8_t *px_map)
+{
+    esp_lcd_panel_handle_t panel_handle = lv_display_get_user_data(disp);
+
+    // This is necessary because LVGL reserves 2 x 4 bytes in the buffer, as these are assumed to be used as a palette. Skip the palette here
+    // More information about the monochrome, please refer to https://docs.lvgl.io/9.2/porting/display.html#monochrome-displays
+    px_map += 8;
+
+    uint16_t hor_res = lv_display_get_physical_horizontal_resolution(disp);
+    int x1 = area->x1;
+    int x2 = area->x2;
+    int y1 = area->y1;
+    int y2 = area->y2;
+
+    for (int y = y1; y <= y2; y++) {
+        for (int x = x1; x <= x2; x++) {
+            /* The order of bits is MSB first
+                        MSB           LSB
+               bits      7 6 5 4 3 2 1 0
+               pixels    0 1 2 3 4 5 6 7
+                        Left         Right
+            */
+            bool chroma_color = (px_map[(hor_res >> 3) * y  + (x >> 3)] & 1 << (7 - x % 8));
+
+            /* Write to the buffer as required for the display.
+            * It writes only 1-bit for monochrome displays mapped vertically.*/
+            uint8_t *buf = oled_buffer + hor_res * (y >> 3) + (x);
+            if (chroma_color) {
+                (*buf) &= ~(1 << (y % 8));
+            } else {
+                (*buf) |= (1 << (y % 8));
+            }
+        }
+    }
+    // pass the draw buffer to the driver
+    esp_lcd_panel_draw_bitmap(panel_handle, x1, y1, x2 + 1, y2 + 1, oled_buffer);
+}
+
+static void example_lvgl_port_task(void *arg)
+{
+    ESP_LOGI(LOG_TAG, "Starting LVGL task");
+    uint32_t time_till_next_ms = 0;
+    while (1) {
+        _lock_acquire(&lvgl_api_lock);
+        time_till_next_ms = lv_timer_handler();
+        _lock_release(&lvgl_api_lock);
+        usleep(1000 * time_till_next_ms);
+    }
+}
+
+static bool example_notify_lvgl_flush_ready(esp_lcd_panel_io_handle_t io_panel, esp_lcd_panel_io_event_data_t *edata, void *user_ctx)
+{
+    lv_display_t *disp = (lv_display_t *)user_ctx;
+    lv_display_flush_ready(disp);
+    return false;
+}
+
+static void example_increase_lvgl_tick(void *arg)
+{
+    /* Tell LVGL how many milliseconds has elapsed */
+    lv_tick_inc(5);
+}
+
+void example_lvgl_demo_ui(lv_display_t *disp)
+{
+    lv_obj_t *scr = lv_display_get_screen_active(disp);
+    lv_obj_t *label = lv_label_create(scr);
+    lv_label_set_long_mode(label, LV_LABEL_LONG_SCROLL_CIRCULAR); /* Circular scroll */
+    lv_label_set_text(label, "Hello Espressif, Hello LVGL.");
+    /* Size of the screen (if you use rotation 90 or 270, please use lv_display_get_vertical_resolution) */
+    lv_obj_set_width(label, lv_display_get_horizontal_resolution(disp));
+    lv_obj_align(label, LV_ALIGN_TOP_MID, 0, 0);
+}
+
+void displayOn()
+{
+  gpio_reset_pin(Vext);
+  gpio_set_direction(Vext, GPIO_MODE_OUTPUT);
+  gpio_set_level(Vext, 0);
+}
+
+void initDisplay()
+{
+
+    ESP_LOGI(LOG_TAG, "Initialize I2C bus");
+    i2c_master_bus_handle_t i2c_bus = NULL;
+    i2c_master_bus_config_t bus_config = {
+        .clk_source = 11,
+        .glitch_ignore_cnt = 7,
+        .i2c_port = 0,
+        .sda_io_num = SDA_OLED,
+        .scl_io_num = SCL_OLED,
+        .flags.enable_internal_pullup = true,
+    };
+    ESP_ERROR_CHECK(i2c_new_master_bus(&bus_config, &i2c_bus));
+
+    ESP_LOGI(LOG_TAG, "Install panel IO");
+    esp_lcd_panel_io_handle_t io_handle = NULL;
+    esp_lcd_panel_io_i2c_config_t io_config = {
+        .dev_addr = I2C_HW_ADDR,
+        .scl_speed_hz = 400000,
+        .control_phase_bytes = 1,               // According to SSD1306 datasheet
+        .lcd_cmd_bits = 8,                      // According to SSD1306 datasheet
+        .lcd_param_bits = 8,                    // According to SSD1306 datasheet
+        .dc_bit_offset = 6,                     // According to SSD1306 datasheet
+    };
+
+    ESP_ERROR_CHECK(esp_lcd_new_panel_io_i2c(i2c_bus, &io_config, &io_handle));
+
+    ESP_LOGI(LOG_TAG, "Install SSD1306 panel driver");
+    esp_lcd_panel_handle_t panel_handle = NULL;
+    esp_lcd_panel_dev_config_t panel_config = {
+        .bits_per_pixel = 1,
+        .reset_gpio_num = RST_OLED,
+    };
+    esp_lcd_panel_ssd1306_config_t ssd1306_config = {
+        .height = DISPLAY_HEIGHT,
+    };
+    panel_config.vendor_config = &ssd1306_config;
+    ESP_ERROR_CHECK(esp_lcd_new_panel_ssd1306(io_handle, &panel_config, &panel_handle));
+
+    ESP_ERROR_CHECK(esp_lcd_panel_reset(panel_handle));
+    ESP_ERROR_CHECK(esp_lcd_panel_init(panel_handle));
+    ESP_ERROR_CHECK(esp_lcd_panel_disp_on_off(panel_handle, true));
+
+    ESP_LOGI(LOG_TAG, "Initialize LVGL");
+    lv_init();
+    // create a lvgl display
+    lv_display_t *display = lv_display_create(DISPLAY_HEIGHT, DISPLAY_WIDTH);
+    // associate the i2c panel handle to the display
+    lv_display_set_user_data(display, panel_handle);
+    // create draw buffer
+    void *buf = NULL;
+    ESP_LOGI(LOG_TAG, "Allocate separate LVGL draw buffers");
+    // LVGL reserves 2 x 4 bytes in the buffer, as these are assumed to be used as a palette.
+    size_t draw_buffer_sz = DISPLAY_HEIGHT * DISPLAY_WIDTH / 8 + 8;
+    buf = heap_caps_calloc(1, draw_buffer_sz, MALLOC_CAP_INTERNAL | MALLOC_CAP_8BIT);
+    assert(buf);
+
+    // LVGL9 suooprt new monochromatic format.
+    lv_display_set_color_format(display, LV_COLOR_FORMAT_I1);
+    // initialize LVGL draw buffers
+    lv_display_set_buffers(display, buf, NULL, draw_buffer_sz, LV_DISPLAY_RENDER_MODE_FULL);
+    // set the callback which can copy the rendered image to an area of the display
+    lv_display_set_flush_cb(display, example_lvgl_flush_cb);
+
+    ESP_LOGI(LOG_TAG, "Register io panel event callback for LVGL flush ready notification");
+    const esp_lcd_panel_io_callbacks_t cbs = {
+        .on_color_trans_done = example_notify_lvgl_flush_ready,
+    };
+    /* Register done callback */
+    esp_lcd_panel_io_register_event_callbacks(io_handle, &cbs, display);
+
+    ESP_LOGI(LOG_TAG, "Use esp_timer as LVGL tick timer");
+    const esp_timer_create_args_t lvgl_tick_timer_args = {
+        .callback = &example_increase_lvgl_tick,
+        .name = "lvgl_tick"
+    };
+    esp_timer_handle_t lvgl_tick_timer = NULL;
+    ESP_ERROR_CHECK(esp_timer_create(&lvgl_tick_timer_args, &lvgl_tick_timer));
+    ESP_ERROR_CHECK(esp_timer_start_periodic(lvgl_tick_timer, 5000));
+
+    ESP_LOGI(LOG_TAG, "Create LVGL task");
+    xTaskCreate(example_lvgl_port_task, "LVGL", 4096, NULL, 2, NULL);
+
+    ESP_LOGI(LOG_TAG, "Display LVGL Scroll Text");
+    // Lock the mutex due to the LVGL APIs are not thread-safe
+    _lock_acquire(&lvgl_api_lock);
+    example_lvgl_demo_ui(display);
+    _lock_release(&lvgl_api_lock);
+}
+
+void setup()
+{
+  displayOn();
+  initDisplay();
+}
 
 void app_main(void)
 {
+  setup();
+
   char* ourTaskName = pcTaskGetName(NULL);
   ESP_LOGI(ourTaskName, "Hello...\n");
 
-  gpio_reset_pin(WHITE_LED);
-  gpio_set_direction(WHITE_LED, GPIO_MODE_OUTPUT);
+  gpio_reset_pin(LED_BUILTIN);
+  gpio_set_direction(LED_BUILTIN, GPIO_MODE_OUTPUT);
 
   while(1)
   {
-    gpio_set_level(WHITE_LED, 0);
+    gpio_set_level(LED_BUILTIN, 0);
     vTaskDelay(1000/portTICK_PERIOD_MS);
-    gpio_set_level(WHITE_LED, 1);
+    gpio_set_level(LED_BUILTIN, 1);
     vTaskDelay(1000/portTICK_PERIOD_MS);
   }
 
