24b93eda76b7a39cc2e33dcd4cc0c9bc1220139f
Your Name <aminvatsal1999@gmail.com>
2025-02-28 00:41:17 -0600

    updates

--- main/main.c
+++ main/main.c
@@ -1,24 +1,65 @@
+
+/*
+ * SPDX-FileCopyrightText: 2021-2024 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: CC0-1.0
+ */
+
 #include <stdio.h>
 #include <unistd.h>
-#include <freertos/FreeRTOS.h>
-#include <freertos/task.h>
-#include "driver/gpio.h"
-#include "esp_log.h"
+#include <sys/lock.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_timer.h"
 #include "esp_lcd_panel_io.h"
 #include "esp_lcd_panel_ops.h"
-#include "esp_lcd_panel_dev.h"
-#include "esp_lcd_panel_ssd1306.h"
-#include "esp_timer.h"
 #include "esp_err.h"
 #include "esp_log.h"
 #include "driver/i2c_master.h"
 #include "lvgl.h"
-
+#include "driver/gpio.h"
 #include "pins.h"
 
-static uint8_t oled_buffer[DISPLAY_WIDTH * DISPLAY_HEIGHT / 8];
+#include "esp_lcd_panel_vendor.h"
+
+static const char *TAG = "example";
+
+#define I2C_BUS_PORT  0
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////// Please update the following configuration according to your LCD spec //////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#define EXAMPLE_LCD_PIXEL_CLOCK_HZ    (400 * 1000)
+#define EXAMPLE_PIN_NUM_SDA           17
+#define EXAMPLE_PIN_NUM_SCL           18
+#define EXAMPLE_PIN_NUM_RST           21
+#define EXAMPLE_I2C_HW_ADDR           0x3C
+
+// The pixel number in horizontal and vertical
+#define EXAMPLE_LCD_H_RES              128
+#define EXAMPLE_LCD_V_RES              64
+// Bit number used to represent command and parameter
+#define EXAMPLE_LCD_CMD_BITS           8
+#define EXAMPLE_LCD_PARAM_BITS         8
+
+#define EXAMPLE_LVGL_TICK_PERIOD_MS    5
+#define EXAMPLE_LVGL_TASK_STACK_SIZE   (4 * 1024)
+#define EXAMPLE_LVGL_TASK_PRIORITY     2
+#define EXAMPLE_LVGL_PALETTE_SIZE      8
+
+// To use LV_COLOR_FORMAT_I1, we need an extra buffer to hold the converted data
+static uint8_t oled_buffer[EXAMPLE_LCD_H_RES * EXAMPLE_LCD_V_RES / 8];
+// LVGL library is not thread-safe, this example will call LVGL APIs from different tasks, so use a mutex to protect it
 static _lock_t lvgl_api_lock;
-static const char *LOG_TAG = "LOG";
+
+extern void example_lvgl_demo_ui(lv_disp_t *disp);
+
+static bool example_notify_lvgl_flush_ready(esp_lcd_panel_io_handle_t io_panel, esp_lcd_panel_io_event_data_t *edata, void *user_ctx)
+{
+    lv_display_t *disp = (lv_display_t *)user_ctx;
+    lv_display_flush_ready(disp);
+    return false;
+}
 
 static void example_lvgl_flush_cb(lv_display_t *disp, const lv_area_t *area, uint8_t *px_map)
 {
@@ -26,7 +67,7 @@ static void example_lvgl_flush_cb(lv_display_t *disp, const lv_area_t *area, uin
 
     // This is necessary because LVGL reserves 2 x 4 bytes in the buffer, as these are assumed to be used as a palette. Skip the palette here
     // More information about the monochrome, please refer to https://docs.lvgl.io/9.2/porting/display.html#monochrome-displays
-    px_map += 8;
+    px_map += EXAMPLE_LVGL_PALETTE_SIZE;
 
     uint16_t hor_res = lv_display_get_physical_horizontal_resolution(disp);
     int x1 = area->x1;
@@ -60,38 +101,23 @@ static void example_lvgl_flush_cb(lv_display_t *disp, const lv_area_t *area, uin
 
 static void example_lvgl_port_task(void *arg)
 {
-    ESP_LOGI(LOG_TAG, "Starting LVGL task");
-    uint32_t time_till_next_ms = 0;
+    ESP_LOGI(TAG, "Starting LVGL task");
     while (1) {
         _lock_acquire(&lvgl_api_lock);
-        time_till_next_ms = lv_timer_handler();
+        vTaskDelay(1000/portTICK_PERIOD_MS);
         _lock_release(&lvgl_api_lock);
-        usleep(1000 * time_till_next_ms);
     }
-}
-
-static bool example_notify_lvgl_flush_ready(esp_lcd_panel_io_handle_t io_panel, esp_lcd_panel_io_event_data_t *edata, void *user_ctx)
-{
-    lv_display_t *disp = (lv_display_t *)user_ctx;
-    lv_display_flush_ready(disp);
-    return false;
-}
-
-static void example_increase_lvgl_tick(void *arg)
-{
-    /* Tell LVGL how many milliseconds has elapsed */
-    lv_tick_inc(5);
 }
 
 void example_lvgl_demo_ui(lv_display_t *disp)
 {
     lv_obj_t *scr = lv_display_get_screen_active(disp);
     lv_obj_t *label = lv_label_create(scr);
-    lv_label_set_long_mode(label, LV_LABEL_LONG_SCROLL_CIRCULAR); /* Circular scroll */
-    lv_label_set_text(label, "Hello Espressif, Hello LVGL.");
+    lv_label_set_long_mode(label, LV_LABEL_LONG_SCROLL); /* Circular scroll */
+    lv_label_set_text(label, "Hello, Testing");
     /* Size of the screen (if you use rotation 90 or 270, please use lv_display_get_vertical_resolution) */
     lv_obj_set_width(label, lv_display_get_horizontal_resolution(disp));
-    lv_obj_align(label, LV_ALIGN_TOP_MID, 0, 0);
+    lv_obj_align(label, LV_ALIGN_TOP_MID, 50, 50);
 }
 
 void displayOn()
@@ -101,42 +127,47 @@ void displayOn()
   gpio_set_level(Vext, 0);
 }
 
-void initDisplay()
+void displayOff()
 {
+  gpio_reset_pin(Vext);
+  gpio_set_direction(Vext, GPIO_MODE_INPUT);
+  gpio_set_level(Vext, 1);
+}
 
-    ESP_LOGI(LOG_TAG, "Initialize I2C bus");
+void app_main(void)
+{
+    ESP_LOGI(TAG, "Initialize I2C bus");
     i2c_master_bus_handle_t i2c_bus = NULL;
     i2c_master_bus_config_t bus_config = {
-        .clk_source = 11,
+        .clk_source = I2C_CLK_SRC_DEFAULT,
         .glitch_ignore_cnt = 7,
-        .i2c_port = 0,
-        .sda_io_num = SDA_OLED,
-        .scl_io_num = SCL_OLED,
+        .i2c_port = I2C_BUS_PORT,
+        .sda_io_num = EXAMPLE_PIN_NUM_SDA,
+        .scl_io_num = EXAMPLE_PIN_NUM_SCL,
         .flags.enable_internal_pullup = true,
     };
     ESP_ERROR_CHECK(i2c_new_master_bus(&bus_config, &i2c_bus));
 
-    ESP_LOGI(LOG_TAG, "Install panel IO");
+    ESP_LOGI(TAG, "Install panel IO");
     esp_lcd_panel_io_handle_t io_handle = NULL;
     esp_lcd_panel_io_i2c_config_t io_config = {
-        .dev_addr = I2C_HW_ADDR,
-        .scl_speed_hz = 400000,
+        .dev_addr = EXAMPLE_I2C_HW_ADDR,
+        .scl_speed_hz = EXAMPLE_LCD_PIXEL_CLOCK_HZ,
         .control_phase_bytes = 1,               // According to SSD1306 datasheet
-        .lcd_cmd_bits = 8,                      // According to SSD1306 datasheet
-        .lcd_param_bits = 8,                    // According to SSD1306 datasheet
+        .lcd_cmd_bits = EXAMPLE_LCD_CMD_BITS,   // According to SSD1306 datasheet
+        .lcd_param_bits = EXAMPLE_LCD_CMD_BITS, // According to SSD1306 datasheet
         .dc_bit_offset = 6,                     // According to SSD1306 datasheet
     };
-
     ESP_ERROR_CHECK(esp_lcd_new_panel_io_i2c(i2c_bus, &io_config, &io_handle));
 
-    ESP_LOGI(LOG_TAG, "Install SSD1306 panel driver");
+    ESP_LOGI(TAG, "Install SSD1306 panel driver");
     esp_lcd_panel_handle_t panel_handle = NULL;
     esp_lcd_panel_dev_config_t panel_config = {
         .bits_per_pixel = 1,
-        .reset_gpio_num = RST_OLED,
+        .reset_gpio_num = EXAMPLE_PIN_NUM_RST,
     };
     esp_lcd_panel_ssd1306_config_t ssd1306_config = {
-        .height = DISPLAY_HEIGHT,
+        .height = EXAMPLE_LCD_V_RES,
     };
     panel_config.vendor_config = &ssd1306_config;
     ESP_ERROR_CHECK(esp_lcd_new_panel_ssd1306(io_handle, &panel_config, &panel_handle));
@@ -145,17 +176,17 @@ void initDisplay()
     ESP_ERROR_CHECK(esp_lcd_panel_init(panel_handle));
     ESP_ERROR_CHECK(esp_lcd_panel_disp_on_off(panel_handle, true));
 
-    ESP_LOGI(LOG_TAG, "Initialize LVGL");
+    ESP_LOGI(TAG, "Initialize LVGL");
     lv_init();
     // create a lvgl display
-    lv_display_t *display = lv_display_create(DISPLAY_WIDTH, DISPLAY_HEIGHT);
+    lv_display_t *display = lv_display_create(EXAMPLE_LCD_H_RES, EXAMPLE_LCD_V_RES);
     // associate the i2c panel handle to the display
     lv_display_set_user_data(display, panel_handle);
     // create draw buffer
     void *buf = NULL;
-    ESP_LOGI(LOG_TAG, "Allocate separate LVGL draw buffers");
+    ESP_LOGI(TAG, "Allocate separate LVGL draw buffers");
     // LVGL reserves 2 x 4 bytes in the buffer, as these are assumed to be used as a palette.
-    size_t draw_buffer_sz = DISPLAY_WIDTH * DISPLAY_HEIGHT / 8 + 8;
+    size_t draw_buffer_sz = EXAMPLE_LCD_H_RES * EXAMPLE_LCD_V_RES / 8 + EXAMPLE_LVGL_PALETTE_SIZE;
     buf = heap_caps_calloc(1, draw_buffer_sz, MALLOC_CAP_INTERNAL | MALLOC_CAP_8BIT);
     assert(buf);
 
@@ -166,54 +197,25 @@ void initDisplay()
     // set the callback which can copy the rendered image to an area of the display
     lv_display_set_flush_cb(display, example_lvgl_flush_cb);
 
-    ESP_LOGI(LOG_TAG, "Register io panel event callback for LVGL flush ready notification");
+    ESP_LOGI(TAG, "Register io panel event callback for LVGL flush ready notification");
     const esp_lcd_panel_io_callbacks_t cbs = {
         .on_color_trans_done = example_notify_lvgl_flush_ready,
     };
     /* Register done callback */
     esp_lcd_panel_io_register_event_callbacks(io_handle, &cbs, display);
 
-    ESP_LOGI(LOG_TAG, "Use esp_timer as LVGL tick timer");
-    const esp_timer_create_args_t lvgl_tick_timer_args = {
-        .callback = &example_increase_lvgl_tick,
-        .name = "lvgl_tick"
-    };
-    esp_timer_handle_t lvgl_tick_timer = NULL;
-    ESP_ERROR_CHECK(esp_timer_create(&lvgl_tick_timer_args, &lvgl_tick_timer));
-    ESP_ERROR_CHECK(esp_timer_start_periodic(lvgl_tick_timer, 5000));
+    gpio_reset_pin(LED_BUILTIN);
+    gpio_set_direction(LED_BUILTIN, GPIO_MODE_OUTPUT);
+  
+    while(1)
+    {
+      vTaskDelay(1000/portTICK_PERIOD_MS);
+      displayOn();
+      gpio_set_level(LED_BUILTIN, 1);
+      example_lvgl_demo_ui(display);
 
-    ESP_LOGI(LOG_TAG, "Create LVGL task");
-    xTaskCreate(example_lvgl_port_task, "LVGL", 4096, NULL, 2, NULL);
-
-    ESP_LOGI(LOG_TAG, "Display LVGL Scroll Text");
-    // Lock the mutex due to the LVGL APIs are not thread-safe
-    _lock_acquire(&lvgl_api_lock);
-    example_lvgl_demo_ui(display);
-    _lock_release(&lvgl_api_lock);
-}
-
-void setup()
-{
-  displayOn();
-  initDisplay();
-}
-
-void app_main(void)
-{
-  setup();
-
-  char* ourTaskName = pcTaskGetName(NULL);
-  ESP_LOGI(ourTaskName, "Hello...\n");
-
-  gpio_reset_pin(LED_BUILTIN);
-  gpio_set_direction(LED_BUILTIN, GPIO_MODE_OUTPUT);
-
-  while(1)
-  {
-    gpio_set_level(LED_BUILTIN, 0);
-    vTaskDelay(1000/portTICK_PERIOD_MS);
-    gpio_set_level(LED_BUILTIN, 1);
-    vTaskDelay(1000/portTICK_PERIOD_MS);
-  }
-
+      vTaskDelay(1000/portTICK_PERIOD_MS);
+      displayOff();
+      gpio_set_level(LED_BUILTIN, 0);
+    }
 }
