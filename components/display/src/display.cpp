6bdda6ba3ae112bcb357986f57d307eefc1e9a90
aminvatsal1999 <aminvatsal1999@gmail.com>
2025-03-13 00:20:52 -0500

    cleanup

--- components/display/src/display.cpp
+++ components/display/src/display.cpp
@@ -2,9 +2,8 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "driver/gpio.h"
-#include "soc/gpio_num.h"
 #include "display.h"
-#include "heltec_constants.h"
+#include "constants/heltec_pins.h"
 
 void turnOffOLED()
 {
@@ -14,18 +13,15 @@ void turnOffOLED()
 
 SSD1306Wire initDisplay()
 {
-    // Turning OLED on
-    gpio_set_direction(GPIO_NUM_36, GPIO_MODE_OUTPUT);
-    gpio_set_level(GPIO_NUM_36, 0);
+    // Turning OLED on (without this, the OLED is very dim)
+    gpio_set_direction(HELTEC_VEXT, GPIO_MODE_OUTPUT);
+    gpio_set_level(HELTEC_VEXT, 0);
 
     // Resetting the OLED
-    gpio_set_direction(GPIO_NUM_21, GPIO_MODE_OUTPUT);
-    vTaskDelay(1000/portTICK_PERIOD_MS);
-    gpio_set_level(GPIO_NUM_21, 1);
-    vTaskDelay(1000/portTICK_PERIOD_MS);
-    gpio_set_level(GPIO_NUM_21, 0);
-    vTaskDelay(1000/portTICK_PERIOD_MS);
-    gpio_set_level(GPIO_NUM_21, 1);    
+    gpio_set_direction(HELTEC_RST_OLED, GPIO_MODE_OUTPUT);
+    vTaskDelay(100/portTICK_PERIOD_MS);
+    gpio_set_level(HELTEC_RST_OLED, 1);
 
-    return SSD1306Wire(0x3C, HELTEC_SDA_OLED, HELTEC_SCL_OLED);  // Adjust based on your wiring
+    // Making the display object and returning it
+    return SSD1306Wire(0x3C, HELTEC_SDA_OLED, HELTEC_SCL_OLED);
 }
--- main/main.cpp
+++ main/main.cpp
@@ -1,14 +1,12 @@
 #include <Arduino.h>
 #include <Wire.h>
-#include "SSD1306Wire.h"
 #include "display.h"
-#include "heltec_constants.h"
 
 extern "C" void app_main() {
     initArduino();
-    auto display = initDisplay();
+    SSD1306Wire display = initDisplay();
     display.init();
     display.setFont(ArialMT_Plain_16);
-    display.drawString(0, 0, "gay ahh clint");
+    display.drawString(0, 0, "testing");
     display.display();
 }
