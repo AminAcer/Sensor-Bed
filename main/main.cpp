0bda63f1f111a440fe80aa5e6d2874fed431ad10
vats <aminvatsal1999@gmail.com>
2025-03-13 00:25:38 -0500

    Fixing modules, cleaning up code through components (#1)
    * trying to fix modules
    
    * cleanup
    
    * component working
    
    * more display component stuff
    
    * more cleanup
    
    * cleanup
    
    * spacing

--- main/main.cpp
+++ main/main.cpp
@@ -1,44 +1,12 @@
 #include <Arduino.h>
-#include <pins.h>
 #include <Wire.h>
-#include "SSD1306Wire.h"
-#include "driver/gpio.h"
-#include "hal/gpio_types.h"
-#include "portmacro.h"
-#include "soc/gpio_num.h"
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-
-SSD1306Wire display(0x3C, VATS_SDA_OLED, VATS_SCL_OLED);  // Adjust based on your wiring
-
-void turnOffOLED()
-{
-    gpio_set_direction(GPIO_NUM_36, GPIO_MODE_OUTPUT);
-    gpio_set_level(GPIO_NUM_36, 1);
-}
-
-void initOLED()
-{
-    // Turning OLED on
-    gpio_set_direction(GPIO_NUM_36, GPIO_MODE_OUTPUT);
-    gpio_set_level(GPIO_NUM_36, 0);
-
-    // Resetting the OLED
-    gpio_set_direction(GPIO_NUM_21, GPIO_MODE_OUTPUT);
-    vTaskDelay(1000/portTICK_PERIOD_MS);
-    gpio_set_level(GPIO_NUM_21, 1);
-    vTaskDelay(1000/portTICK_PERIOD_MS);
-    gpio_set_level(GPIO_NUM_21, 0);
-    vTaskDelay(1000/portTICK_PERIOD_MS);
-    gpio_set_level(GPIO_NUM_21, 1);    
-}
+#include "display.h"
 
 extern "C" void app_main() {
     initArduino();
-    initOLED();
-
+    SSD1306Wire display = initDisplay();
     display.init();
     display.setFont(ArialMT_Plain_16);
-    display.drawString(0, 0, "gay ahh clint");
+    display.drawString(0, 0, "testing");
     display.display();
 }
